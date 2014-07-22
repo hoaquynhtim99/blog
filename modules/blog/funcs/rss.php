94e33f5f7e5ce5c8f8d5edf32ba3dddd4ee2355f
Phan Tan Dung <phantandung92@gmail.com>
2014-07-22 12:30:56 +0700

    Cập nhật chức năng RSS.

--- modules/blog/funcs/rss.php
+++ modules/blog/funcs/rss.php
@@ -7,69 +7,79 @@
  * @Createdate Dec 11, 2013, 09:50:11 PM
  */
 
-if ( ! defined( 'NV_IS_MOD_FILM' ) ) die( 'Stop!!!' );
+if( ! defined( 'NV_IS_MOD_BLOG' ) ) die( 'Stop!!!' );
 
 $channel = array();
 $items = array();
 
-$channel['title'] = $global_config['site_name'] . ' RSS: ' . $module_info['custom_title'];
+$channel['title'] = $module_info['custom_title'] . ' ' . NV_TITLEBAR_DEFIS . ' ' . $global_config['site_name'] . ' RSS';
 $channel['link'] = NV_MY_DOMAIN . NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module_name;
 $channel['atomlink'] = NV_MY_DOMAIN . NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module_name . "&amp;" . NV_OP_VARIABLE . "=rss";
-$channel['description'] = $global_config['site_description'];
+$channel['description'] = $module_info['description'] ? $module_info['description'] : $global_config['site_description'];
 
-if ( ! empty( $nv_film_listcat ) )
+if( ! empty( $global_array_cat ) )
 {
-    $catalias = isset( $array_op[1] ) ? $array_op[1] : "";
-    $catid = 0;
-    
-    if ( ! empty( $catalias ) )
-    {
-        foreach ( $nv_film_listcat as $c )
-        {
-            if ( $c['alias'] == $catalias )
-            {
-                $catid = $c['id'];
-                break;
-            }
-        }
-    }
-    
-    if ( $catid > 0 )
-    {
-        $channel['title'] = $global_config['site_name'] . ' RSS: ' . $module_info['custom_title'] . ' - ' . $nv_film_listcat[$catid]['title'];
-        $channel['link'] = NV_MY_DOMAIN . NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module_name . "&amp;cat=" . $nv_film_listcat[$catid]['alias'];
-        $channel['description'] = $nv_film_listcat[$catid]['description'];
-        
-        $sql = "SELECT `id`, `catid`, `addtime`, `title`, `introtext`, `description`, `images`
-        FROM `" . NV_PREFIXLANG . "_" . $module_data . "` WHERE `catid`=" . $catid . " 
-        AND `status`=1 ORDER BY `addtime` DESC LIMIT 30";
-    }
-    else
-    {
-        $in = array_keys( $nv_film_listcat );
-        $in = implode( ",", $in );
-        $sql = "SELECT `id`, `catid`, `addtime`, `title`, `introtext`, `description`, `images` 
-        FROM `" . NV_PREFIXLANG . "_" . $module_data . "` WHERE `catid` IN (" . $in . ") 
-        AND `status`=1 ORDER BY `addtime` DESC LIMIT 30";
-    }
-    if ( $module_info['rss'] )
-    {
-        if ( ( $result = $db->sql_query( $sql ) ) !== false )
-        {
-            while ( list( $id, $cid, $publtime, $title, $hometext, $description, $images ) = $db->sql_fetchrow( $result ) )
-            {
-				$hometext = empty( $hometext ) ? nv_clean60( strip_tags( $description ), 300 ) : $hometext;
-                $rimages = ( ! empty( $images ) ) ? "<img src=\"" . NV_MY_DOMAIN . NV_BASE_SITEURL . NV_UPLOADS_DIR . $images . "\" width=\"100\" align=\"left\" border=\"0\">" : "";
-                $items[] = array(
-                    'title' => $title,
-					'link' => NV_MY_DOMAIN . NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module_name . "&amp;" . NV_OP_VARIABLE . "=" . $nv_film_listcat[$cid]['alias'] . "/" . change_alias( $title . "-" . $id ),
-					'guid' => $module_name . '_' . $id,
-					'description' => $rimages . $hometext,
-					'pubdate' => $publtime
-                );
-            }
-        }
-    }
+	$catalias = isset( $array_op[1] ) ? $array_op[1] : "";
+	$catid = 0;
+	
+	if( ! empty( $catalias ) )
+	{
+		foreach ( $global_array_cat as $c )
+		{
+			if( $c['alias'] == $catalias )
+			{
+				$catid = $c['id'];
+				break;
+			}
+		}
+	}
+	
+	if( $catid > 0 )
+	{
+		$channel['title'] = $global_array_cat[$catid]['title'] . ' ' . NV_TITLEBAR_DEFIS . ' ' . $module_info['custom_title'] . ' ' . NV_TITLEBAR_DEFIS . ' ' . $global_config['site_name'] . ' RSS';
+		$channel['link'] = NV_MY_DOMAIN . NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module_name . "&amp;" . NV_OP_VARIABLE . "=" . $global_array_cat[$catid]['alias'];
+		$channel['description'] = $global_array_cat[$catid]['description'];
+		
+		$sql = "SELECT `id`, `title`, `alias`, `images`, `pubTime`, `hometext` FROM `" . $BL->table_prefix . "_rows` WHERE ( " . $BL->build_query_search_id( $catid, 'catids' ) . " ) AND `status`=1 ORDER BY `pubTime` DESC LIMIT 30";
+	}
+	else
+	{
+		$sql = "SELECT `id`, `title`, `alias`, `images`, `pubTime`, `hometext` FROM `" . $BL->table_prefix . "_rows` WHERE `status`=1 ORDER BY `pubTime` DESC LIMIT 30";
+	}
+	
+	if( $module_info['rss'] )
+	{
+		if( ( $result = $db->sql_query( $sql ) ) !== false )
+		{
+			while( $row = $db->sql_fetch_assoc( $result ) )
+			{
+				// Xac dinh images
+				if( ! empty( $row['images'] ) )
+				{
+					if( is_file( NV_UPLOADS_REAL_DIR . '/' . $module_name . $row['images'] ) )
+					{
+						$row['images'] = '<img src="' . NV_MY_DOMAIN . NV_BASE_SITEURL . NV_UPLOADS_DIR . '/' . $module_name . $row['images'] . '" alt="' . $row['title'] . '" width="100" style="float:left;margin-right:10px"/>';
+					}
+					elseif( nv_is_url( $row['images'] ) )
+					{
+						$row['images'] = '<img src="' . $row['images'] . '" alt="' . $row['title'] . '" width="100" style="float:left;margin-right:10px"/>';
+					}
+					else
+					{
+						$row['images'] = '';
+					}
+				}
+			
+				$items[] = array(
+					'title' => $row['title'],
+					'link' => NV_MY_DOMAIN . NV_BASE_SITEURL . "index.php?" . NV_LANG_VARIABLE . "=" . NV_LANG_DATA . "&amp;" . NV_NAME_VARIABLE . "=" . $module_name . "&amp;" . NV_OP_VARIABLE . "=" . $row['alias'],
+					'guid' => $module_name . '_' . $row['id'],
+					'description' => $row['images'] . $row['hometext'],
+					'pubdate' => $row['pubTime']
+				);
+			}
+		}
+	}
 }
 
 nv_rss_generate( $channel, $items );
