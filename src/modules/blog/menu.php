a9438c4a6741a0f3a26956446cfa3da18ad18010
Phan Tấn Dũng <dungpt@vinades.vn>
2024-08-16 07:53:24 +0700

    Sửa lỗi lấy menu

--- src/modules/blog/menu.php
+++ src/modules/blog/menu.php
@@ -12,7 +12,7 @@ if (!defined('NV_IS_FILE_ADMIN')) {
     die('Stop!!!');
 }
 
-$sql = "SELECT * FROM " . NV_PREFIXLANG . "_" . $mod_data . "_categories ORDER BY parentid,weight ASC";
+$sql = "SELECT * FROM " . NV_PREFIXLANG . "_" . $mod_data . "_categories ORDER BY weight_all ASC";
 $result = $db->query($sql);
 
 while ($row = $result->fetch()) {
